291b4b748030c9975ca2f062b3c8bfeec4fda0d1
Matt Winzer <matt-winzer@users.noreply.github.com>
2019-09-06 11:43:32 -0600

    Created filterStructure.js and added content

---
+++ filterStructure.js
@@ -0,0 +1,9 @@
+// ------------ FILTER STRUCTURE ------------
+
+// array to be filtered
+var numbers = [1, 2, 3, 4, 5];
+
+// filtering and storing result in variable
+var filteredNums = numbers.filter(function(number, i, originalArr) {
+  // filtering logic...
+});
